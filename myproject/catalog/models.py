00934973203694cbd2b27e7696ce41cc3061598e
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-05-06 23:47:32 +0800

    Basic settings of the Product and Category model

--- myproject/catalog/models.py
+++ myproject/catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Модель категории."""
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        """Отображаемое имя модели в единственном и множественном числе."""
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        """Строковое представление категории."""
+        return self.name
+
+
+class Product(models.Model):
+    """Модель продукта."""
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='products/', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Отображаемое имя модели в единственном и множественном числе."""
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        """Строковое представление продукта."""
+        return self.name
