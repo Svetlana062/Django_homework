fd330061aeef00262fc50ab8bf61655daf84e79a
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-05-06 23:53:37 +0800

    Custom command for adding test products

---
+++ myproject/catalog/management/commands/add_test_products.py
@@ -0,0 +1,25 @@
+import os
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from django.conf import settings
+
+# Импортируем модели из приложения catalog
+from catalog.models import Product, Category
+
+
+class Command(BaseCommand):
+    help = 'Load test data into the database'
+
+    def handle(self, *args, **kwargs):
+        # Удаляем существующие данные перед загрузкой новых данных.
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        # Загружаем фиктуры.
+        fixture_path = os.path.join(settings.BASE_DIR, 'category_fixture.json')  # Путь к фиктуре
+
+        try:
+            call_command('loaddata', fixture_path)
+            self.stdout.write(self.style.SUCCESS('Тестовые данные успешно загружены!'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Ошибка при загрузке данных: {e}'))
