2b2410c0c0f62db15efb7ec5c69ce7a5dfabb674
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-05-14 15:50:11 +0800

    settings added

--- myproject/catalog/urls.py
+++ myproject/catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from django.views.generic import TemplateView
 
@@ -8,4 +10,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
     path('contacts/success/', TemplateView.as_view(template_name='catalog/contact_success.html'), name='contact_success'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myproject/catalog/views.py
+++ myproject/catalog/views.py
@@ -1,22 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()
+    return render(request, 'catalog/home.html', {'products': products})
 
 def contacts(request):
-    return render(request, 'catalog/contacts.html')
-
-def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            # После успешной обработки перенаправляем на страницу успеха
-            print(form.cleaned_data)  # Выводим данные в консоль
-
-            return render(request, 'catalog/contact_success.html')
+            # Обработка данных формы
+            print(form.cleaned_data)  # Для теста
+            return redirect('contact_success')  # Название URL для страницы успеха
     else:
         form = ContactForm()
 
     return render(request, 'catalog/contacts.html', {'form': form})
+
+def contact_success(request):
+    return render(request, 'catalog/contact_success.html')
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
+
