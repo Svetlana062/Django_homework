ec46ad4d7059fcdef38ef24ea3972b0ca3d9d845
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-05-14 15:35:48 +0800

    a feedback form has been created

--- myproject/catalog/forms.py
+++ myproject/catalog/forms.py
@@ -1,7 +1,27 @@
 from django import forms
 
-
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Ваше имя', max_length=100)
-    email = forms.EmailField(label='Ваш email')
-    message = forms.CharField(label='Сообщение', widget=forms.Textarea)
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ваше имя',
+            'required': True,
+        })
+    )
+    phone = forms.CharField(
+        max_length=20,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Контактный телефон',
+            'required': True,
+        })
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите текст',
+            'rows': 4,
+            'required': True,
+        })
+    )
