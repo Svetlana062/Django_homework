71455b1493b4d9c4134ee8cf45e9e6d442350abe
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-05-18 14:34:58 +0800

    A new blog post model has been created

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200) # заголовок
+    content = models.TextField() # содержимое
+    preview_image = models.ImageField(upload_to='previews/') # превью (изображение)
+    created_at = models.DateTimeField(auto_now_add=True) # дата создания
+    is_published = models.BooleanField(default=False) # признак публикации (булевое поле)
+    views_count = models.PositiveIntegerField(default=0) # количество просмотров
+
+    def __str__(self):
+        return self.title
